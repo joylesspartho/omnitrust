b43d2c0defffae49b1c64eb39e134eab847b0b84
Joyless Partho <joylesspartho@gmail.com>
2025-12-22 18:00:59 +0600

    update img

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -4,12 +4,13 @@ export default function ContactMap() {
   return (
     <div className="w-full h-[400px] md:h-[500px] rounded-3 overflow-hidden shadow">
       <iframe
-        title="OmniTrust Properties Ltd Location"
-        src="https://www.google.com/maps?q=23.779441101619216,90.39293012759335&z=16&output=embed"
+        title="Omnitrust Properties Ltd Location"
+        src="https://www.google.com/maps?q=23.779561078603383,90.392955610954&z=18&output=embed"
         width="100%"
         height="100%"
         style={{ border: 0 }}
         loading="lazy"
+        allowFullScreen
         referrerPolicy="no-referrer-when-downgrade"
       />
     </div>
